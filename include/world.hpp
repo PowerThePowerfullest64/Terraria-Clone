ae7dc5b244edbc16395438f38959fc496471cfcf
PowerThePowerfullest64 <andersmb2008@gmail.com>
2026-06-15 16:37:59 +0200

    World Size Type Change

--- include/world.hpp
+++ include/world.hpp
@@ -13,7 +13,7 @@ class Game;
 
 class World {
 public:
-    static constexpr uint16_t WIDTH = 512, HEIGHT = 24;
+    static constexpr int WIDTH = 512, HEIGHT = 24;
 
     // Returns a texture based on the given type, like a map.
     Texture2D* blockTextures[COUNT];
