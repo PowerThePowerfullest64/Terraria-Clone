92b17f4fc12b324cbdc154783e9f310b4437fd3c
Ducc08 <andersmb2008@gmail.com>
2026-07-07 01:52:35 +0200

    Turned Debug Off

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,13 +1,13 @@
 #include "settings.hpp"
 
 // Debug
-bool Settings::showChunkBorders = true;
+bool Settings::showChunkBorders = false;
 bool Settings::showMouseBlockType = true;
 bool Settings::showMouseCoords = true;
 bool Settings::showFPS = true;
 bool Settings::showPlayerVelocity = false;
 bool Settings::showPlayerColliders = false;
-bool Settings::showActivelyMinedBlocks = true;
+bool Settings::showActivelyMinedBlocks = false;
 
 // Physics
 float Settings::gravity = 1500.f;
